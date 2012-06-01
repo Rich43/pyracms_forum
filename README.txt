799523e38b4ab6a82d3069c093c2af89aecce93d
Richie Ward <RichieS@GMail.com>
2012-06-01 13:39:43 +0100

    Add initial project

---
+++ README.txt
@@ -0,0 +1,14 @@
+pyracms_forum README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $venv/bin/python setup.py develop
+
+- $venv/bin/populate_pyracms_forum development.ini
+
+- $venv/bin/pserve development.ini
+
---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'SQLAlchemy',
+    'transaction',
+    'pyramid_tm',
+    'pyramid_debugtoolbar',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='pyracms_forum',
+      version='0.0',
+      description='pyracms_forum',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='pyracms_forum',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = pyracms_forum:main
+      [console_scripts]
+      initialize_pyracms_forum_db = pyracms_forum.scripts.initializedb:main
+      """,
+      )
+
